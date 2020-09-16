332e814840a340d44154da7a687e14b356b5328c
cuiWant <505521261@qq.com>
2020-09-16 16:03:13 +0800

    feat:'add iconfont '

--- vue.config.js
+++ vue.config.js
@@ -12,27 +12,29 @@ module.exports = {
 		proxy: {
 			'/api': {
 				//这里最好有一个 /
-				target: 'http://localhost:4000', // 后台接口域名
+				target: 'http://172.16.16.9', // 后台接口域名
 				//ws: true,        //如果要代理 websockets，配置这个参数
 				// secure: false, // 如果是https接口，需要配置这个参数
 				changeOrigin: true, //是否跨域
 				pathRewrite: {
-					'^/api': ''
+					// '^/api': ''
 				}
 			},
-			'/server': {
-				//这里最好有一个 /
-				target: 'http://localhost:4000', // 后台接口域名
-				changeOrigin: true, //是否跨域
-				pathRewrite: {
-					'^/server': ''
-				}
-			},
-			'/soket.io': {
-				target: 'http://localhost:4000', // 后台接口域名
-				changeOrigin: true, //是否跨域
-				ws: true
-			}
+			// '/api': {
+			// 	//这里最好有一个 /
+			// 	target: 'http://localhost:4000', // 后台接口域名
+			// 	//ws: true,        //如果要代理 websockets，配置这个参数
+			// 	// secure: false, // 如果是https接口，需要配置这个参数
+			// 	changeOrigin: true, //是否跨域
+			// 	pathRewrite: {
+			// 		'^/api': ''
+			// 	}
+			// },
+			// '/soket.io': {
+			// 	target: 'http://localhost:4000', // 后台接口域名
+			// 	changeOrigin: true, //是否跨域
+			// 	ws: true
+			// }
 		}
 	}
 };
